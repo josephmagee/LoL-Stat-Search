3c21dfacdcd9e94852f8f4d0b6938789fe3ff41e
Joseph Magee <josephlmagee@gmail.com>
2016-07-23 17:34:43 -0400

    baseline commit of basic application

---
+++ README.md
@@ -0,0 +1,33 @@
+## Synopsis
+
+LoL Stats is a web application in which you can search for relevant player stats, such as their most played champion and their KDA.
+
+## Motivation
+
+LoL Stats exists as a weekend project that I wanted to utilize the Riot Games API in. It allowed me to create a page using the Handlebars templating engine, nodeJS, and focuses around manipulating information from the API.
+
+## API Reference
+
+[Riot Games API located here](https://developer.riotgames.com/)
+
+## MIT License
+
+Copyright (c) [2016] [Joseph Magee]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars');
+var request = require('request');
+var async = require('async');
+var fs = require('fs');
+/*
+ * get your own config file you crazy kids, and stay off my lawn. configuration.json holds your API key from Riot.
+ * your API key can be found at https://developer.riotgames.com/ after logging into your league of legends account.
+ */
+var configuration = JSON.parse(
+    fs.readFileSync('./data/configuration.json'));
+
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+app.get('/search', function(req, res) {
+    var path = './data/logs.json';
+    var obj = JSON.parse(fs.readFileSync(path, 'utf8'));
+    var data = {};
+    var api_key = configuration.apikey;
+    var s_toSearch = req.query.summoner.toLowerCase().replace(/ /g, '');
+
+    async.waterfall([
+            function(callback) {
+                var URL = 'https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + s_toSearch + '?api_key=' + api_key;
+                request(URL, function(err, response, body) {
+                    if (!err && response.statusCode == 200) {
+                        var json = JSON.parse(body);
+                        data.id = json[s_toSearch].id;
+                        data.name = json[s_toSearch].name;
+                        callback(null, data);
+                    } else {
+                        console.log(err);
+                    }
+                });
+            },
+            function(data, callback) {
+                var URL = 'https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/' + data.id + '/ranked?api_key=' + api_key;
+                request(URL, function(err, response, body) {
+                    if (!err && response.statusCode == 200) {
+                        var json = JSON.parse(body);
+                        var mostPlayed = 0;
+                        var championPlayed = 0;
+                        for (var c = 0; c < json['champions'].length; c++) {
+                            var currentPlayed = json['champions'][c]['stats'].totalSessionsPlayed;
+                            if (currentPlayed > mostPlayed && !json['champions'][c].id == 0) {
+                                mostPlayed = currentPlayed;
+                                championPlayed = json['champions'][c].id;
+                            }
+                        }
+                        data.champId = championPlayed;
+                        data.timesPlayed = mostPlayed;
+                        callback(null, data);
+                    } else {
+                        console.log(err);
+                    }
+                });
+            },
+            function(data, callback) {
+                var URL = 'https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion/' + data.champId + '?locale=en_US&api_key=' + api_key;
+                request(URL, function(err, response, body) {
+                    if (response.statusCode == 200) {
+                        var json = JSON.parse(body);
+                        data.champName = json['name'];
+                        callback(null, data);
+                    } else {
+                        console.log(err);
+                    }
+                });
+            }
+        ],
+        function(err, data) {
+            if(err) {
+                console.log(err);
+                res.render('index');
+                return;
+            }
+            obj.searches.push({
+                "value": data.name
+            });
+            fs.writeFile(path, JSON.stringify(obj, null, ''), function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+            res.render('index', {
+                info: data
+            });
+        }
+    );
+});
+
+var port = Number(process.env.PORT || 3000);
+app.listen(port);
